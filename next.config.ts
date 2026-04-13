f4240264922ae20921ef4d6f1c0e75e0b8c775fb
Charlsz <110142703+Charlsz@users.noreply.github.com>
2026-04-13 11:44:45 -0500

    feat: implement dynamic base path for media and links based on environment

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,10 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: "/my-website",
+  basePath: isProd ? "/my-website" : "",
   trailingSlash: true,
   images: {
     unoptimized: true,
